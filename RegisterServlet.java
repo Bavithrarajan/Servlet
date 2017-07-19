090ef6d572ca16fc11ed285aa1d7fe7e02b299c4
Bavithrarajan <bavithrarajan.be@gmail.com>
2017-07-19 10:55:05 +0530

    Add files via upload

---
+++ RegisterServlet.java
@@ -0,0 +1,59 @@
+package com.book;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookapp.dao.UserDAO;
+import com.bookapp.model.User;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+	String name = request.getParameter("name");
+	String emailId = request.getParameter("email_id");
+	String password = request.getParameter("password");
+	
+	User user = new User();
+	user.setName(name);
+	user.setEmailId(emailId);
+	user.setPassword(password);
+	
+	UserDAO dao = new UserDAO();
+	dao.register(user);
+	response.sendRedirect("home.html");
+	
+	
+	}
+
+}
